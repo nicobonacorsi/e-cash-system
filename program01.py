c80b481d5c15ee92d6cf69d898d51241823aaa72
Nicolò Bonacorsi <142901284+nicobonacorsi@users.noreply.github.com>
2023-08-22 16:16:32 +0200

    Update program01.py

--- program01.py
+++ program01.py
@@ -1,194 +1,160 @@
-
 def ex1(acn1, acn2, acn3, imd_acn1, imd_acn2, init_amount, transact_log):
-    # creo dizionario per inizializzare l'importo iniziale
-    saldi_giocatori = {acn1: init_amount, 
-                       acn2: init_amount, 
+    # Create a dictionary to initialize initial amounts
+    player_balances = {acn1: init_amount,
+                       acn2: init_amount,
                        acn3: init_amount}
     
-    # creo dizionario per inizializzare i saldi degli intermediari
-    saldi_intermediari = {imd_acn1: 0, 
-                          imd_acn2: 0}
+    # Create a dictionary to initialize intermediary balances
+    intermediary_balances = {imd_acn1: 0,
+                             imd_acn2: 0}
     
-    # creo dizionario per inizializzare l'importo iniziale dei debiti nei confornti degli intermediair
-    debiti = {imd_acn1: {acn1: 0, acn2: 0, acn3: 0}, 
+    # Create a dictionary to initialize initial debt amounts for intermediaries
+    debts = {imd_acn1: {acn1: 0, acn2: 0, acn3: 0},
               imd_acn2: {acn1: 0, acn2: 0, acn3: 0}}
     
-    #creo una fuzione che mi distribuisca i soldi del saldo del mittente per ricoprire i debiti
-    def distribuisci_soldi(saldo_giocatore, debito_imdacn1,debito_imdacn2,saldo_intermediario1,saldo_intermediario2):
-        mini_da_coprire=max(abs(debito_imdacn1),abs(debito_imdacn2))
+    # Create a function to distribute sender's balance to cover debts
+    def distribute_money(player_balance, imd_debt1, imd_debt2, imd_balance1, imd_balance2):
+        min_to_cover = max(abs(imd_debt1), abs(imd_debt2))
 
-        if mini_da_coprire !=0:
+        if min_to_cover != 0:
             
-            #seil saldo è diverso da zero
-            if saldo_giocatore !=0:
-            #se il saldo è maggiore o uguale del primo debito che inizio a coprire
-                if saldo_giocatore>=mini_da_coprire:
-                #se il debito 1 è uguale a zero e quindi inizio a coprire l'altro
-                    if debito_imdacn1 == 0:
-                        debito_imdacn2 = 0
-                        
-                        saldo_intermediario2 += mini_da_coprire
-                        saldo_giocatore -=mini_da_coprire
-                #se il debito 2 è uguale a zero e quindi inizio a coprire l'altro
-                    elif debito_imdacn2==0:
-                        debito_imdacn1 = 0
-                        saldo_giocatore -=mini_da_coprire
-
-                        saldo_intermediario1 +=mini_da_coprire
-                   
-                    elif debito_imdacn1 == debito_imdacn2 and debito_imdacn1 != 0 :
-                        #Entrambi i debiti sono uguali, copri entrambi in modo equo
-                        if saldo_giocatore>=2*(mini_da_coprire):
-                             
-                            debito_imdacn1 = 0
-                            debito_imdacn2 =0
-                            
-                            saldo_intermediario1 += mini_da_coprire
-                            saldo_intermediario2 += mini_da_coprire
-                            saldo_giocatore -= (mini_da_coprire * 2)
+            # If balance is not zero
+            if player_balance != 0:
+                # If balance is greater or equal to the first debt to cover
+                if player_balance >= min_to_cover:
+                    if imd_debt1 == 0:
+                        imd_debt2 = 0
+                        imd_balance2 += min_to_cover
+                        player_balance -= min_to_cover
+                    elif imd_debt2 == 0:
+                        imd_debt1 = 0
+                        player_balance -= min_to_cover
+                        imd_balance1 += min_to_cover
+                    elif imd_debt1 == imd_debt2 and imd_debt1 != 0:
+                        if player_balance >= 2 * min_to_cover:
+                            imd_debt1 = 0
+                            imd_debt2 = 0
+                            imd_balance1 += min_to_cover
+                            imd_balance2 += min_to_cover
+                            player_balance -= 2 * min_to_cover
                         else:
-                            quota_da_coprire = saldo_giocatore
-                            debito_imdacn1 += (quota_da_coprire/2)
-                            debito_imdacn2 += quota_da_coprire/2
-                            saldo_intermediario1 += quota_da_coprire/2
-                            saldo_intermediario2 += quota_da_coprire/2
-                            saldo_giocatore =0
-                    elif debito_imdacn1 != debito_imdacn2 and debito_imdacn1 != 0 and debito_imdacn2 != 0:
-                        if abs(debito_imdacn1)>abs(debito_imdacn2):
-                    
-                            saldo_intermediario1 += abs(debito_imdacn1)
-                            saldo_giocatore -=  abs(debito_imdacn1)
-                            debito_imdacn1=0
-                            nuovo_min=min(saldo_giocatore,abs(debito_imdacn2))
-
-                            debito_imdacn2+=nuovo_min
-                            saldo_intermediario2 += nuovo_min
-                            saldo_giocatore -=nuovo_min
-                            
-                        elif abs(debito_imdacn1)<abs(debito_imdacn2):
-                            saldo_giocatore-=abs(debito_imdacn2)
-                            saldo_intermediario2 += abs(debito_imdacn2)
-                            debito_imdacn2=0
-                            nuovo_min=min(saldo_giocatore,abs(debito_imdacn1))
-
-                            debito_imdacn1 +=nuovo_min
-                            saldo_intermediario1 += nuovo_min
-                            saldo_giocatore -=nuovo_min
-                            
-                # se saldo< primo debito da coprire
+                            to_cover_share = player_balance
+                            imd_debt1 += to_cover_share / 2
+                            imd_debt2 += to_cover_share / 2
+                            imd_balance1 += to_cover_share / 2
+                            imd_balance2 += to_cover_share / 2
+                            player_balance = 0
+                    elif imd_debt1 != imd_debt2 and imd_debt1 != 0 and imd_debt2 != 0:
+                        if abs(imd_debt1) > abs(imd_debt2):
+                            imd_balance1 += abs(imd_debt1)
+                            player_balance -= abs(imd_debt1)
+                            imd_debt1 = 0
+                            new_min = min(player_balance, abs(imd_debt2))
+                            imd_debt2 += new_min
+                            imd_balance2 += new_min
+                            player_balance -= new_min
+                        elif abs(imd_debt1) < abs(imd_debt2):
+                            player_balance -= abs(imd_debt2)
+                            imd_balance2 += abs(imd_debt2)
+                            imd_debt2 = 0
+                            new_min = min(player_balance, abs(imd_debt1))
+                            imd_debt1 += new_min
+                            imd_balance1 += new_min
+                            player_balance -= new_min
+                # If balance < first debt to cover
                 else:
-                    
-                    if debito_imdacn1 == 0:
-                        debito_imdacn2 +=saldo_giocatore
-                        
-                        saldo_intermediario2 += saldo_giocatore
-                        saldo_giocatore=0
-                #se il debito 2 è uguale a zero e quindi inizio a coprire l'altro
-                    elif debito_imdacn2==0:
-                        debito_imdacn1 +=saldo_giocatore
-                        
-                        saldo_intermediario1 += saldo_giocatore
-                        saldo_giocatore =0
-                   
-                    elif abs(debito_imdacn1 )== abs(debito_imdacn2) and debito_imdacn1 != 0 :
-                        quota_da_coprire = saldo_giocatore
-                        debito_imdacn1 += (quota_da_coprire/2)
-                        debito_imdacn2 += (quota_da_coprire/2)
-                        saldo_giocatore -= quota_da_coprire
-                        saldo_intermediario1 += (quota_da_coprire/2)
-                        saldo_intermediario2 += (quota_da_coprire/2)
-                    elif debito_imdacn1 != debito_imdacn2 and debito_imdacn1 != 0 and debito_imdacn2 != 0:
-                        if abs(debito_imdacn1)>abs(debito_imdacn2):
-                            debito_imdacn1+=saldo_giocatore
-                            saldo_intermediario1 += saldo_giocatore
-                            saldo_giocatore =0
-                            debito_imdacn2=debito_imdacn2
-                            saldo_intermediario2=saldo_intermediario2
-                            
-                        
-                        elif abs(debito_imdacn1)<abs(debito_imdacn2):
-                            debito_imdacn2+=saldo_giocatore
-                            saldo_intermediario2 += saldo_giocatore
-                            saldo_giocatore =0
-                            debito_imdacn1=debito_imdacn1
-                            saldo_intermediario1=saldo_intermediario1
-                    return saldo_giocatore, debito_imdacn1, debito_imdacn2, saldo_intermediario1, saldo_intermediario2
-  
-# se entrambi i debiti sono uguali a zero ritorna senza modificare
+                    if imd_debt1 == 0:
+                        imd_debt2 += player_balance
+                        imd_balance2 += player_balance
+                        player_balance = 0
+                    elif imd_debt2 == 0:
+                        imd_debt1 += player_balance
+                        imd_balance1 += player_balance
+                        player_balance = 0
+                    elif abs(imd_debt1) == abs(imd_debt2) and imd_debt1 != 0:
+                        to_cover_share = player_balance
+                        imd_debt1 += to_cover_share / 2
+                        imd_debt2 += to_cover_share / 2
+                        player_balance -= to_cover_share
+                        imd_balance1 += to_cover_share / 2
+                        imd_balance2 += to_cover_share / 2
+                    elif imd_debt1 != imd_debt2 and imd_debt1 != 0 and imd_debt2 != 0:
+                        if abs(imd_debt1) > abs(imd_debt2):
+                            imd_debt1 += player_balance
+                            imd_balance1 += player_balance
+                            player_balance = 0
+                            imd_debt2 = imd_debt2
+                            imd_balance2 = imd_balance2
+                        elif abs(imd_debt1) < abs(imd_debt2):
+                            imd_debt2 += player_balance
+                            imd_balance2 += player_balance
+                            player_balance = 0
+                            imd_debt1 = imd_debt1
+                            imd_balance1 = imd_balance1
+            return player_balance, imd_debt1, imd_debt2, imd_balance1, imd_balance2
+        return player_balance, imd_debt1, imd_debt2, imd_balance1, imd_balance2
 
-            return saldo_giocatore, debito_imdacn1, debito_imdacn2,saldo_intermediario1,saldo_intermediario2               
-        return saldo_giocatore, debito_imdacn1, debito_imdacn2,saldo_intermediario1,saldo_intermediario2               
+        
 
-    # Iterazione sulle transazioni nel registro che sonoa loro volta liste
-    #for transazione in transact_log:
-    for transazione in transact_log:
-        (mittente, destinatario) = transazione[0]
-        importo = transazione[1]
-        conto_intermediario = transazione[2]
-        commissione = transazione[3]
+    # Iteration over transactions in the log, each being a list
+    for transaction in transact_log:
+        (sender, recipient) = transaction[0]
+        amount = transaction[1]
+        intermediary_account = transaction[2]
+        commission = transaction[3]
         
-        saldo_giocatore = saldi_giocatori[mittente]
-        debito_imdacn2 = debiti[imd_acn2][mittente]
-        debito_imdacn1 = debiti[imd_acn1][mittente]
+        player_balance = player_balances[sender]
+        imd_debt2 = debts[imd_acn2][sender]
+        imd_debt1 = debts[imd_acn1][sender]
         
-        saldo_intermediario1 = saldi_intermediari[imd_acn1]
-        saldo_intermediario2 = saldi_intermediari[imd_acn2]
-    
-        importo_commissione=(importo*(commissione/100))
+        imd_balance1 = intermediary_balances[imd_acn1]
+        imd_balance2 = intermediary_balances[imd_acn2]
         
-        saldo_giocatore, debito_imdacn1, debito_imdacn2, saldo_intermediario1, saldo_intermediario2 = distribuisci_soldi(saldi_giocatori[mittente], debiti[imd_acn1][mittente], debiti[imd_acn2][mittente],saldi_intermediari[imd_acn1], saldi_intermediari[imd_acn2])
+        commission_amount = amount * (commission / 100)
         
-        saldi_giocatori[mittente] = saldo_giocatore
-        debiti[imd_acn1][mittente] = debito_imdacn1
-        debiti[imd_acn2][mittente] = debito_imdacn2
-        saldi_intermediari[imd_acn1] = saldo_intermediario1
-        saldi_intermediari[imd_acn2] = saldo_intermediario2
-        #CASO 1: se  il saldo del giocatore è maggiore o uguale di (importo+importo_commissione)
-        if saldi_giocatori[mittente] >= (importo + importo_commissione):
-            saldi_giocatori[mittente] -= (importo + importo_commissione)
-            saldi_giocatori[destinatario] += importo
-            saldi_intermediari[conto_intermediario] += importo_commissione
+        player_balance, imd_debt1, imd_debt2, imd_balance1, imd_balance2 = distribute_money(player_balances[sender], debts[imd_acn1][sender], debts[imd_acn2][sender], intermediary_balances[imd_acn1], intermediary_balances[imd_acn2])
+        
+        player_balances[sender] = player_balance
+        debts[imd_acn1][sender] = imd_debt1
+        debts[imd_acn2][sender] = imd_debt2
+        intermediary_balances[imd_acn1] = imd_balance1
+        intermediary_balances[imd_acn2] = imd_balance2
+        
+        # CASE 1: If sender's balance is greater than or equal to (amount + commission_amount)
+        if player_balances[sender] >= (amount + commission_amount):
+            player_balances[sender] -= (amount + commission_amount)
+            player_balances[recipient] += amount
+            intermediary_balances[intermediary_account] += commission_amount
             
-            saldo_giocatore, debito_imdacn1, debito_imdacn2, saldo_intermediario1, saldo_intermediario2 = distribuisci_soldi(
-            saldi_giocatori[destinatario], debiti[imd_acn1][destinatario], debiti[imd_acn2][destinatario],
-            saldi_intermediari[imd_acn1], saldi_intermediari[imd_acn2])
+            player_balance, imd_debt1, imd_debt2, imd_balance1, imd_balance2 = distribute_money(
+            player_balances[recipient], debts[imd_acn1][recipient], debts[imd_acn2][recipient],
+            intermediary_balances[imd_acn1], intermediary_balances[imd_acn2])
 
-            saldi_giocatori[destinatario] = saldo_giocatore
-            debiti[imd_acn1][destinatario] = debito_imdacn1
-            debiti[imd_acn2][destinatario] = debito_imdacn2
-            saldi_intermediari[imd_acn1] = saldo_intermediario1
-            saldi_intermediari[imd_acn2] = saldo_intermediario
-                
-      
-        #CASO 2: se il saldo del giocatore è compreso tra importo_commmissione  e importo+importo_commissione
-        #in questo caso al destinatario non arriva nulla e arriva solo all'intermediario
+            player_balances[recipient] = player_balance
+            debts[imd_acn1][recipient] = imd_debt1
+            debts[imd_acn2][recipient] = imd_debt2
+            intermediary_balances[imd_acn1] = imd_balance1
+            intermediary_balances[imd_acn2] = imd_balance2
         
-        elif (importo + importo_commissione)>saldi_giocatori[mittente]>= importo_commissione:
-            saldi_giocatori[mittente]=saldi_giocatori[mittente]-importo_commissione
-            
-            saldi_intermediari[conto_intermediario]=saldi_intermediari[conto_intermediario]+ importo_commissione
-            
+        # CASE 2: If sender's balance is between commission_amount and amount + commission_amount
+        elif (amount + commission_amount) > player_balances[sender] >= commission_amount:
+            player_balances[sender] = player_balances[sender] - commission_amount
+            intermediary_balances[intermediary_account] = intermediary_balances[intermediary_account] + commission_amount
     
-        #CASO 3 : se il saldo del giocatore è minore di importo_commissione arrivano tutti i soldi all'intermediario e giocatore rimane in debito negativo
-        #in questo caso gestisco i debiti
-
-        elif saldi_giocatori[mittente]<importo_commissione :
-            saldi_intermediari[conto_intermediario]=saldi_intermediari[conto_intermediario]+saldi_giocatori[mittente]
-            debito_rimanente = saldi_giocatori[mittente] - importo_commissione
-            debiti[conto_intermediario][mittente]=debiti[conto_intermediario][mittente] + debito_rimanente
-            saldi_giocatori[mittente]-= saldi_giocatori[mittente]
+        # CASE 3: If sender's balance is less than commission_amount, all money goes to the intermediary and sender goes into negative debt
+        elif player_balances[sender] < commission_amount:
+            intermediary_balances[intermediary_account] = intermediary_balances[intermediary_account] + player_balances[sender]
+            remaining_debt = player_balances[sender] - commission_amount
+            debts[intermediary_account][sender] = debts[intermediary_account][sender] + remaining_debt
+            player_balances[sender] = player_balances[sender] - player_balances[sender]
             
-                
-    # Creazione delle liste di saldo finale dei giocatori
-    saldi_finali_giocatori = [int(saldi_giocatori[acn1]),int(saldi_giocatori[acn2]), int(saldi_giocatori[acn3])]
-    # Creazione delle liste di importo finale guadagnato dagli intermediari
-    importi_finali_intermediari = [int(saldi_intermediari[imd_acn1]), int(saldi_intermediari[imd_acn2])]
-    # Creazione delle liste di debiti residui dei conti del giocatore per gli intermediari
-    debiti_residui = [[int(debiti[imd_acn1][acn1]), int(debiti[imd_acn1][acn2]), int(debiti[imd_acn1][acn3])],
-        [int(debiti[imd_acn2][acn1]), int(debiti[imd_acn2][acn2]), int(debiti[imd_acn2][acn3])]]
+    # Creating lists of final player balances
+    final_player_balances = [int(player_balances[acn1]), int(player_balances[acn2]), int(player_balances[acn3])]
+    # Creating lists of final intermediary earnings
+    final_intermediary_earnings = [int(intermediary_balances[imd_acn1]), int(intermediary_balances[imd_acn2])]
+    # Creating lists of remaining debts for player accounts to intermediaries
+    remaining_debts = [[int(debts[imd_acn1][acn1]), int(debts[imd_acn1][acn2]), int(debts[imd_acn1][acn3])],
+        [int(debts[imd_acn2][acn1]), int(debts[imd_acn2][acn2]), int(debts[imd_acn2][acn3])]]
         
-    
-    
-  
-    # Restituzione dei risultati come tupla
-    return (saldi_finali_giocatori, importi_finali_intermediari, debiti_residui)   
+    # Returning results as a tuple
+    return (final_player_balances, final_intermediary_earnings, remaining_debts)
